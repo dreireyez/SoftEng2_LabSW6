fb0f071c6d79d2884c084893f93f2ce0ae587f1b
Djem Andreif Reyes <djemandreif@gmail.com>
2026-04-07 01:27:16 +0000

    Modified the print statements to create distinction between the prototypes and the cloned animals.

--- src/TestAnimal.java
+++ src/TestAnimal.java
@@ -1,3 +1,4 @@
+// Based on original TestAnimal.java by Prof. Jeremias Esperanza
 public class TestAnimal {
     public static void main(String[] args) {
         AnimalRegistry registry = new AnimalRegistry();
@@ -5,27 +6,32 @@ public class TestAnimal {
         Animal cow = registry.createCow();
         Animal horse = registry.createHorse();
         
+        System.out.println("ORIGINAL PROTOTYPES");
         ((Sheep) sheep).setName("Dolly");
         ((Cow) cow).setSound("Moo!");
         ((Horse) horse).setColor("Black");
 
-        System.out.println(sheep.getType() + " named " + ((Sheep) sheep).getName());
-        System.out.println(cow.getType() + " sounds " + ((Cow) cow).getSound());
-        System.out.println(horse.getType() + " color is " + ((Horse) horse).getColor());
+        System.out.println("The original " + sheep.getType() + " is named " + ((Sheep) sheep).getName());
+        System.out.println("The original " + cow.getType() + " says " + ((Cow) cow).getSound());
+        System.out.println("The original " +horse.getType() + " is " + ((Horse) horse).getColor());
+
+        System.out.println();
 
         Animal sheep2 = registry.createSheep();
         ((Sheep) sheep2).setName("Molly");
 
-        System.out.println(sheep2.getType() + " named " + ((Sheep) sheep2).getName());
+        System.out.println("CLONED ANIMALS");
+
+        System.out.println("The cloned " + sheep2.getType() + " is named " + ((Sheep) sheep2).getName());
 
         Animal cow2 = registry.createCow();
         ((Cow) cow2).setFood("Grass");
         ((Cow) cow2).setSound("Maa!");
-        System.out.println(cow2.getType() + " sounds " + ((Cow) cow2).getSound());
-        System.out.println(cow2.getType() + " eats " + ((Cow) cow2).getFood());
+        System.out.println("The cloned " + cow2.getType() + " sounds " + ((Cow) cow2).getSound());
+        System.out.println("The cloned " + cow2.getType() + " eats " + ((Cow) cow2).getFood());
         
         Animal horse2 = registry.createHorse();
         ((Horse) horse2).setColor("Brown");
-        System.out.println(horse2.getType() + " is " + ((Horse) horse2).getColor());
+        System.out.println("The cloned " + horse2.getType() + " is " + ((Horse) horse2).getColor());
     }
 }
